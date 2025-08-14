a4e13f6cc1910c3e3172a1f626f1a864ef3f0215
jcosta.wayon <jcosta.wayon@veloe.com.br>
2025-08-13 21:17:48 -0300

    feat: add product-list and product-item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
 import Header from "@/components/commons/header";
+import ProductList from "@/components/commons/product-list";
+import { db } from "@/db";
+import Image from "next/image"
 
-export default function Home() {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true
+    }
+  });
+ 
   return (
     <>
       <Header></Header>
+
+      <div className="space-y-6 px-5">
+        <Image 
+          src="/banner-01.png"
+          alt="Leve uma vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full "
+        />
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <Image 
+          src="/banner-02.png"
+          alt="Leve uma vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full "
+        />
+
+      </div>
     </>
   );
 }
+
+export default Home;
